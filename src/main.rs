40917ea12e032f737572caec3688b620954b5108
Daniel Hjertholm <danielhjertholm@Daniels-MacBook-Air.local>
2023-03-21 18:06:59 +0100

    Works on my machine

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,47 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use] extern crate rocket;
 
-#[cfg(test)] mod tests;
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+
+
+
 
 #[get("/")]
-fn hello() -> &'static str {
+fn index() -> &'static str {
     "Hello, world!"
 }
 
-fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+#[get("/fix?<msg>")]
+async fn fix(msg: String) -> String {
+    dotenv().ok();
+    let prompt = "Tell me a fun fact about technology.";
+    let client = reqwest::Client::new();
+
+    //https://stackoverflow.com/questions/47911513/how-do-i-set-the-request-headers-using-reqwest
+    let response = client
+        .post("https://api.openai.com/v1/completions")
+        .header("Content-Type", "application/json")
+        .header("Authorization", &format!("Bearer {}", env::var("API_KEY").unwrap()))
+        .json(&serde_json::json!({
+            "model": "text-davinci-003",
+            "prompt": prompt,
+            "max_tokens": 7,
+            "temperature": 0
+          }))
+        .send()
+        .await
+        .unwrap()
+        .text()  // Use json()
+        .await
+        .unwrap();
+
+    println!("{:?}", response);
+    String::from(msg)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, fix])
 }

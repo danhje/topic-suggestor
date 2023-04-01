930204f5dbc011ee144e967e3de0b18c2ddc1ee6
Daniel Hjertholm <8713259+danhje@users.noreply.github.com>
2023-04-01 22:40:32 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![feature(proc_macro_hygiene, decl_macro)]
+#![feature(const_fn_trait_bound)]
 
 #[macro_use] extern crate rocket;
 
@@ -12,9 +13,39 @@ fn index() -> &'static str {
 
 #[get("/fix?<msg>")]
 fn fix(msg: String) -> String {
-    format!("Hello, {msg}!")
+    println!("Hello, {msg}!");
+
+    let prompt = "Tell me a fun fact about technology.";
+
+    let headers = {
+        let mut headers = HeaderMap::new();
+        headers.insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+        headers.insert(AUTHORIZATION, HeaderValue::from_str(&format!("Bearer {}", &api_key)).unwrap());
+        headers
+    };
+
+    let client = reqwest::Client::new();
+    let response = client
+        .post("https://api.openai.com/v1/completions")
+        .headers(headers)
+        .json(&serde_json::json!({
+            "model": "text-davinci-003",
+            "prompt": prompt,
+            "max_tokens": 7,
+            "temperature": 0
+          }))
+        .send()
+        .await
+        .unwrap()
+        .json::<OpenAIResponse>()
+        .await
+        .unwrap();
+
+    response.choices[0].text.clone()
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     rocket::ignite().mount("/", routes![index, fix]).launch();
+
 }

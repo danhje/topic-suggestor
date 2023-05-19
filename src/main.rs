5fd6d2e41242e8f3ce2d19731dfded69b4436fa3
Daniel Hjertholm <daniel.hjertholm@statnett.no>
2023-05-19 14:43:41 +0200

    Remove duplicates

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,20 @@ mod fetch;
 
 #[get("/")]
 fn index() -> String {
-    fs::read_topics().join("\n")
+    fs::read_topics("topics.txt").join("\n")
 }
 
 
 #[get("/pop")]
 fn pop() -> String {
-    fs::pop_topic().unwrap()
+    fs::pop_topic("topics.txt").unwrap()
 }
 
 
 #[get("/extend")]
 async fn extend() -> String {
     let new_suggestions = fetch::parse_suggestions(fetch::fetch_new_suggestions().await);
-    fs::append_topics(&new_suggestions).unwrap();
+    fs::append_topics(&new_suggestions, "topics.txt").unwrap();
     "Fetched the following topics, which have been added to the list of upcoming topics: \n\n".to_string() + &new_suggestions.join("\n")
 }
 
